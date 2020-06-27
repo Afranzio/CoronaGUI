3b704859d94ae2bb69bf5a39d6f033eb6a60b591
Afranzio <61235427+Afranzio@users.noreply.github.com>
2020-06-27 12:08:21 +0530

    Add files via upload

--- Scrapping.py
+++ Scrapping.py
@@ -1,15 +1,20 @@
+#Country Value get from source file
 def count(Country):
+
+    #Import Required Modules to Use..
     import matplotlib.pyplot as plt
     import requests
     import json
     import os
     from pathlib import Path
-
+    
+    #Set This Path As OS Path
     my_file = Path("./pythonjseel/web/img.png")
 
-    if os.path.exists(my_file):
+    if os.path.exists(my_file):  #Check if the file exists and remove it
         os.remove(my_file)
 
+    #API Section
     url = "https://corona-virus-world-and-india-data.p.rapidapi.com/api"
 
     headers = {
@@ -19,14 +24,18 @@ def count(Country):
 
     response = requests.request("GET", url, headers=headers)
 
+    #Getting Value Of Global Status
     world_data = json.loads(response.text)["world_total"]
 
+    #Getting Value Of Every Country Status
     whole_data = json.loads(response.text)["countries_stat"]
 
     country_data = dict
 
+    #Check If Input Country is empty string or not....
     if Country == "" :
         
+        #Extract individual data from API
         Total_Cases = str(world_data["total_cases"])
         Total_Deaths = str(world_data["total_deaths"])
         Total_Recovered = str(world_data["total_recovered"])
@@ -35,17 +44,24 @@ def count(Country):
         Serious_Critical = str(world_data["serious_critical"])
         # Total_Active = int(Total_Cases.replace(",","")) - int(Total_Deaths.replace(",","")) - int(Total_Recovered.replace(",",""))
 
+        #Convert the string data to an integer  to use the data
         plot_Cases = Total_Cases.replace(",","")
         plot_Recovered = int(Total_Recovered.replace(",",""))
         plot_Deaths = int(Total_Deaths.replace(",",""))
 
+        #List those data into a variable
         data = [plot_Cases,plot_Recovered,plot_Deaths]
         label=['Cases,''Recovered','Deaths']
         color = ['royal blue,'"#62D726","red"]
         
+        #Pie Plot the Data
         plt.pie(x=data,startangle=90,labels=label,radius=2,counterclock=False,colors=color,shadow = True)
 
+        #Save the Plot
         plt.savefig(r"C:\\Users\\Afranzio\\Python\\pythonjseel\\web\\ img.png" ,transparent=True,bbox_inches='tight')
+
+        #Clear the previous plot
+        plt.clf()
         
         return("Starts_Over = " + "Global Status",
             "\nTotal_Cases = " + Total_Cases,   
@@ -59,10 +75,13 @@ def count(Country):
         
         
     else:
+
+        #Get the exact country data from all country data
         for data in whole_data:
             if data["country_name"] == Country :
                 country_data = data
 
+        #Extract individual data from API
         Country_Name = country_data["country_name"]
         Total_Cases = str(country_data["cases"])
         Total_Active = str(country_data["active_cases"])
@@ -72,17 +91,24 @@ def count(Country):
         New_Deaths = str(country_data["new_deaths"])
         Total_test = str(country_data["total_tests"])
 
+        #Convert the string data to an integer  to use the data
         plot_Active = int(Total_Active.replace(",",""))
         plot_Recovered = int(Total_Recovered.replace(",",""))
         plot_Deaths = int(Total_Deaths.replace(",",""))
 
+        #List those data into a variable
         data = [plot_Active,plot_Recovered,plot_Deaths]
         label=['Active','Recovered','Deaths']
         color = ["royalblue","#62D726","red"]
-        
+
+        #Plot the Data
         plt.pie(x=data,startangle=90,labels=label,radius=2,explode=[0,0,1],counterclock=False,colors=color,shadow = True,autopct='%1.f%%')
 
+        #Save the plot
         plt.savefig(r"C:\\Users\\Afranzio\\Python\\pythonjseel\\web\\img.png" ,transparent=True, bbox_inches='tight')
+
+        #Clear the plot.
+        plt.clf()
 
         return("Country = " + Country_Name,
             "\nTotal_Cases = " + Total_Cases,
--- source.py
+++ source.py
@@ -1,6 +1,7 @@
 from Scrapping import  count
 import eel
 
+##Select The Folder of FrontEnd File
 eel.init("web")
 
 @eel.expose
@@ -10,13 +11,15 @@ def get(inputs):
     #customize the Inputs
 
     if len(inputs)<=3:
-        inputs = inputs.upper() # for countries like USA,UAE
+        inputs = inputs.upper()  #For countries like USA,UAE
         
     else:
-        inputs = inputs.title() #Others in Title
+        inputs = inputs.title()  #Others in Title
 
     Country = inputs
     
+    ##Pass Country name to the defined function!!
+
     return (count(Country))
 
-eel.start('index.html',size = (1024,500))
+eel.start('index.html',size = (1024,500),cmdline_args=['--start-fullscreen'])
